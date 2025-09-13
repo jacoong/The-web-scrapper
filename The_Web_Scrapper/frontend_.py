6f9210897377ff4246b395cbb899146391694806
jacoong <87224594+jacoong@users.noreply.github.com>
2025-09-14 03:59:49 +0900

    s

--- The_Web_Scrapper/frontend_.py
+++ The_Web_Scrapper/frontend_.py
@@ -166,4 +166,7 @@ def sorry():
         
 
     
-app.run(debug=True, port=5003)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5003))
+    app.run(host="0.0.0.0", port=port, debug=False)
